be322db465347da15fb8855b43b6e0ead00decfc
AbayomiOlaoye <mindprepopps@gmail.com>
2025-01-09 06:09:28 +0100

    Refactor ESLint configuration and simplify Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,4 @@
-export const Loading = ({ name }) => {
-  if (name) {
-    console.log('Loading', name);
-  }
-
+export const Loading = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <div className="h-32 w-32 rounded-full border-b-2 border-gray-900">
